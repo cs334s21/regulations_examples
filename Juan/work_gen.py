55db882ce9cd7a8bfc42e4668fe8961498d39553
Juan <juanpsram@gmail.com>
2021-03-19 14:50:14 -0400

    need to repair the comment query

--- Juan/work_gen.py
+++ Juan/work_gen.py
@@ -1,51 +1,46 @@
 import random
-import time
-import redis
+import time, os
 from utility_functions import\
     get_key, write_all_agencies_ids_to_file,\
     write_all_docketsIDs_in_agency_to_file,\
     write_all_documentsIDs_in_docket_to_file,\
-    write_all_commentsIDs_in_document_to_file
+    write_all_commentsIDs_in_document_to_file,\
+    get_umf
 
-    
-
-def generate_jobs(database, start_key=None):
-    if start_key is None:
-        start_key = random.randint(0, 10)
-    for _ in range(10):
-        value = random.randint(0, 10)
-        print(f"I am generating work {start_key} with value {value}!")
-        database.hset("jobs_waiting", start_key, value)
-        start_key += 1
-
-
-def emulate_job_creation(database):
-    for current_key in range(1, 50, 10):
-        generate_jobs(database, current_key)
-        time.sleep(30)
+# upper most folder
+umf = get_umf()
 
 def job_creation():
     key = get_key()
-    write_all_agencies_ids_to_file(key)
-    with open("all_agencies_ids.txt", 'r') as r:
+    if not os.path.exists(f"./{umf}"):
+        os.makedirs(umf)
+    if not os.path.exists(f"./{umf}/all_agencies_ids.txt"):
+        write_all_agencies_ids_to_file(key)
+    with open(f"{umf}/all_agencies_ids.txt", 'r') as r:
         for agency in r.readlines():
             agency = agency.strip()
-            write_all_docketsIDs_in_agency_to_file(key, agency)
-            with open("docketsIDs_in_" + agency + ".txt", 'r') as r1:
+            agency_dir = f"{umf}/{agency}"
+            if not os.path.exists(f"./{agency_dir}"):
+                os.makedirs(agency_dir)
+            if not os.path.exists(f"./{agency_dir}/docketsIDs_in_{agency}.txt"):
+                write_all_docketsIDs_in_agency_to_file(key, agency_dir,  agency)
+            with open(f"{umf}/{agency}/docketsIDs_in_{agency}.txt", 'r') as r1:
                 for docket in r1.readlines():
                     docket = docket.strip()
-                    write_all_documentsIDs_in_docket_to_file(key, docket)
-                    with open("documentsIDS_in_" + docket + ".txt", 'r') as r2:
+                    docket_dir = f"{umf}/{agency}/{docket}" 
+                    if not os.path.exists(f"./{docket_dir}"):
+                        os.makedirs(docket_dir)
+                    if not os.path.exists(f"./{docket_dir}/documentsIDs_in_{docket}.txt"):
+                        write_all_documentsIDs_in_docket_to_file(key, docket_dir, docket)
+                    with open(f"{docket_dir}/documentsIDs_in_{docket}.txt", 'r') as r2:
                         for document in r2.readlines():
-                            write_all_commentsIDs_in_document_to_file(key, document.strip()) 
+                            document = document.strip()
+                            document_dir = f"{docket_dir}/{document}"
+                            if not os.path.exists(f"./{document_dir}"):
+                                os.makedirs(document_dir)
+                            if not os.path.exists(f"./{document_dir}/commentsIDs_in_{document}.txt"):
+                                write_all_commentsIDs_in_document_to_file(key, document_dir, document) 
 
 
 if __name__ == "__main__":
-    # redis = redis.Redis()
-    # try:
-    #     redis.ping()
-    #     print('Successfully connected to redis')
-    #     emulate_job_creation(redis)
-    # except redis.exceptions.ConnectionError as r_con_error:
-    #     print('Redis connection error:', r_con_error)
     job_creation()
